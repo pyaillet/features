2ec8c52a703a4778cc65056ac455b12640f04655
Pierre-Yves Aillet <pyaillet@users.noreply.github.com>
2025-04-10 19:50:56 +0000

    feat: new features

---
+++ src/helix/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'helix'"
+
+VERSION=${VERSION:-25.01.1}
+ARCH=$(uname -m)
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+PKG=helix-${VERSION}-${ARCH}-${OS}
+
+mkdir /tmp/helix-install
+cd /tmp/helix-install
+curl -LO "https://github.com/helix-editor/helix/releases/download/${VERSION}/${PKG}.tar.xz"
+tar xJf "${PKG}.tar.xz"
+mv "${PKG}/hx" /usr/local/bin/
+mkdir -p /home/${_REMOTE_USER}/.config/helix
+mv "${PKG}/runtime" /home/${_REMOTE_USER}/.config/helix
+chown -R ${_REMOTE_USER}:${_REMOTE_GROUP} /home/${_REMOTE_USER}/.config/helix
+
+echo "Activated feature 'helix'"
---
+++ src/lima-vm/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'lima-vm'"
+
+TMP_DIR=$(mktemp)
+cd $TMP_DIR
+git clone https://github.com/lima-vm/lima
+cd lima
+make
+make install
+cd /
+rm -Rf "$TMP_DIR"
+
+echo "Activated feature 'lima-vm'"
