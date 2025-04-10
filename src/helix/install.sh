c9b6cbf08398a69932c54c0ff3f856b880fdbbe8
Pierre-Yves Aillet <pyaillet@users.noreply.github.com>
2025-04-10 20:05:49 +0000

    fix: tentative

--- src/helix/install.sh
+++ src/helix/install.sh
@@ -16,5 +16,6 @@ mv "${PKG}/hx" /usr/local/bin/
 mkdir -p /home/${_REMOTE_USER}/.config/helix
 mv "${PKG}/runtime" /home/${_REMOTE_USER}/.config/helix
 chown -R ${_REMOTE_USER}:${_REMOTE_GROUP} /home/${_REMOTE_USER}/.config/helix
+rm -Rf /tmp/helix-install
 
 echo "Activated feature 'helix'"
--- src/lima-vm/install.sh
+++ src/lima-vm/install.sh
@@ -4,12 +4,13 @@ set -e
 echo "Activating feature 'lima-vm'"
 
 TMP_DIR=$(mktemp)
-cd $TMP_DIR
+mkdir "${TMP_DIR}"
+cd "${TMP_DIR}"
 git clone https://github.com/lima-vm/lima
 cd lima
 make
 make install
 cd /
-rm -Rf "$TMP_DIR"
+rm -Rf "${TMP_DIR}"
 
 echo "Activated feature 'lima-vm'"
