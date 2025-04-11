4ea24696f3a112cef3a2714bdafb186332acd65a
Pierre-Yves Aillet <pyaillet@users.noreply.github.com>
2025-04-11 05:51:48 +0000

    fix: install helix from source

--- src/helix/install.sh
+++ src/helix/install.sh
@@ -3,19 +3,16 @@ set -e
 
 echo "Activating feature 'helix'"
 
-VERSION=${VERSION:-25.01.1}
-ARCH=$(uname -m)
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-PKG=helix-${VERSION}-${ARCH}-${OS}
+TMP_DIR=$(mktemp -d)
+cd ${TMP_DIR}
+git clone https://github.com/helix-editor/helix
+cd helix
+cargo install --path helix-term --locked
 
-mkdir /tmp/helix-install
-cd /tmp/helix-install
-curl -LO "https://github.com/helix-editor/helix/releases/download/${VERSION}/${PKG}.tar.xz"
-tar xJf "${PKG}.tar.xz"
-mv "${PKG}/hx" /usr/local/bin/
 mkdir -p /home/${_REMOTE_USER}/.config/helix
-mv "${PKG}/runtime" /home/${_REMOTE_USER}/.config/helix
+cp -R runtime /home/${_REMOTE_USER}/.config/helix
 chown -R ${_REMOTE_USER}:${_REMOTE_GROUP} /home/${_REMOTE_USER}/.config/helix
-rm -Rf /tmp/helix-install
+cd /
+rm -Rf "${TMP_DIR}"
 
 echo "Activated feature 'helix'"
